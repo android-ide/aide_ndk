14433da756b9fdea86f8314ab8e72d19ff83def2
Hans Kratz <hans@appfour.com>
2013-04-20 14:24:47 +0000

    document steps to be executed manually in separate file

--- install-prerequisites-ubuntu.sh
+++ install-prerequisites-ubuntu.sh
@@ -5,6 +5,3 @@ TOP=$(cd $SCRIPTDIR; pwd)
 
 aptitude install git build-essential rsync bison flex curl texinfo libtinfo-dev \
 gcc-arm-linux-gnueabi g++-arm-linux-gnueabi xapt
-
-#xapt -a armel -S precise -m --mirror http://ports.ubuntu.com/ libtinfo-dev
-#dpkg-reconfigure dash # make /bin/sh point to bash
