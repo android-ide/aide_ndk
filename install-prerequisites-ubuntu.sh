e2ecc38b1f292654e604914b1dd435f83100b41a
Hans Kratz <hans@appfour.com>
2013-04-20 14:04:38 +0000

    shell script for installing prerequisites in ubuntu

---
+++ install-prerequisites-ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPTDIR=$(dirname $0)
+TOP=$(cd $SCRIPTDIR; pwd)
+
+aptitude install git build-essential rsync bison curl texinfo libtinfo-dev \
+gcc-arm-linux-gnueabi g++-arm-linux-gnueabi xapt
+
+xapt -a armel -S precise -m --mirror http://ports.ubuntu.com/ libtinfo-dev
+
+dpkg-reconfigure dash # make /bin/sh point to bash
