c2fef75e2746c9a2dbcd379dd3547e3b7de359e0
Hans Kratz <hans@appfour.com>
2013-04-20 14:17:39 +0000

    install flex, don't run xapt/dpkg-reconfigure dash every time (xapt is not idempotent)

--- install-prerequisites-ubuntu.sh
+++ install-prerequisites-ubuntu.sh
@@ -3,9 +3,8 @@
 SCRIPTDIR=$(dirname $0)
 TOP=$(cd $SCRIPTDIR; pwd)
 
-aptitude install git build-essential rsync bison curl texinfo libtinfo-dev \
+aptitude install git build-essential rsync bison flex curl texinfo libtinfo-dev \
 gcc-arm-linux-gnueabi g++-arm-linux-gnueabi xapt
 
-xapt -a armel -S precise -m --mirror http://ports.ubuntu.com/ libtinfo-dev
-
-dpkg-reconfigure dash # make /bin/sh point to bash
+#xapt -a armel -S precise -m --mirror http://ports.ubuntu.com/ libtinfo-dev
+#dpkg-reconfigure dash # make /bin/sh point to bash
