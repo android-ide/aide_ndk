46ad184a1c85ce4c517f01b166c5a9a4258eedbd
Hans Kratz <hans@appfour.com>
2013-04-20 14:05:16 +0000

    fix: don't use absolute path in build script

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 SCRIPTDIR=$(dirname $0)
 TOP=$(cd $SCRIPTDIR; pwd)
 NDK=$TOP/platform/ndk
 NDK_LOGFILE=/tmp/ndk-build.log
 export NDK NDK_LOGFILE NUM_JOBS
 rm $NDK_LOGFILE
-$NDK/build/tools/rebuild-all-prebuilt.sh --arch=arm --systems=linux-x86 --package-dir=$TOP/ndk-prebuilt ~/ndk-develop/toolchain-sources/
-$NDK/build/tools/rebuild-all-prebuilt.sh --systems=armstatic --arch=arm --package-dir=$TOP/ndk-prebuilt ~/ndk-develop/toolchain-sources/
+$NDK/build/tools/rebuild-all-prebuilt.sh --arch=arm --systems=linux-x86 --package-dir=$TOP/ndk-prebuilt $TOP/toolchain-sources/
+$NDK/build/tools/rebuild-all-prebuilt.sh --systems=armstatic --arch=arm --package-dir=$TOP/ndk-prebuilt $TOP/toolchain-sources/
