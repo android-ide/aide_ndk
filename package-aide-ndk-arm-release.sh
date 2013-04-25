f897ecdbfb7620bf2154cb6caf567948857a4569
Hans Kratz <hans@appfour.com>
2013-04-25 07:03:40 +0000

    fix busybox upload path

--- package-aide-ndk-arm-release.sh
+++ package-aide-ndk-arm-release.sh
@@ -12,6 +12,6 @@ rm -rf android-ndk-aide/{samples,docs,tests}
 tar cjf $TOP/ndk-aide-release/android-ndk-aide-linux-arm-$RELEASE.tar.bz2 android-ndk-aide/
 cp $TOP/busybox/busybox $TOP/ndk-aide-release/busybox-$RELEASE
 cp $TOP/ndkinstall.sh $TOP/ndk-aide-release/ndkinstall.sh-$RELEASE
-python $TOP/googlecode_upload.py -s "Busybox for arm" -p aide-support $TOP/ndk-aide-release/busybox-$RELEASE
+python $TOP/googlecode_upload.py -s "Busybox for arm" -p aide-support $TOP/ndk-aide-release/busybox-arm-$RELEASE
 python $TOP/googlecode_upload.py -s "AIDE NDK Support installation script" -p aide-support $TOP/ndk-aide-release/ndkinstall.sh-$RELEASE
 python $TOP/googlecode_upload.py -s "NDK for arm (static)" -p aide-support $TOP/ndk-aide-release/android-ndk-aide-linux-arm-$RELEASE.tar.bz2
