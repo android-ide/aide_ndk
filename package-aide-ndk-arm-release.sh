8322691e83c52eddbf49ef834fcc8acf6ad70fde
Hans Kratz <hans@appfour.com>
2013-04-25 07:15:42 +0000

    fix busybox path on Google Code

--- package-aide-ndk-arm-release.sh
+++ package-aide-ndk-arm-release.sh
@@ -10,7 +10,7 @@ tar xjf  $TOP/ndk-release/android-ndk-aide-linux-arm.tar.bz2
 #rm -rf $TOP/ndk-aide-prebuilt/tmp
 rm -rf android-ndk-aide/{samples,docs,tests}
 tar cjf $TOP/ndk-aide-release/android-ndk-aide-linux-arm-$RELEASE.tar.bz2 android-ndk-aide/
-cp $TOP/busybox/busybox $TOP/ndk-aide-release/busybox-$RELEASE
+cp $TOP/busybox/busybox $TOP/ndk-aide-release/busybox-arm-$RELEASE
 cp $TOP/ndkinstall.sh $TOP/ndk-aide-release/ndkinstall.sh-$RELEASE
 python $TOP/googlecode_upload.py -s "Busybox for arm" -p aide-support $TOP/ndk-aide-release/busybox-arm-$RELEASE
 python $TOP/googlecode_upload.py -s "AIDE NDK Support installation script" -p aide-support $TOP/ndk-aide-release/ndkinstall.sh-$RELEASE
