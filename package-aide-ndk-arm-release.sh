befda7e8bb76089ccb35fecb8c50c426bb44131d
Hans Kratz <hans@appfour.com>
2013-05-06 17:07:21 +0000

    remove remains of unsupported toolchains to force proper error message

--- package-aide-ndk-arm-release.sh
+++ package-aide-ndk-arm-release.sh
@@ -9,6 +9,13 @@ cd $TOP/ndk-aide-release/tmp
 tar xjf  $TOP/ndk-release/android-ndk-aide-linux-arm.tar.bz2
 #rm -rf $TOP/ndk-aide-prebuilt/tmp
 rm -rf android-ndk-aide/{samples,docs,tests}
+for toolchain in android-ndk-aide/toolchains/*
+do
+  if [ "${toolchain}" != "android-ndk-aide/toolchains/arm-linux-androideabi-4.6" ]
+  then
+    rm -r ${toolchain}
+  fi
+done
 tar cjf $TOP/ndk-aide-release/android-ndk-aide-linux-arm-$RELEASE.tar.bz2 android-ndk-aide/
 cp $TOP/busybox/busybox $TOP/ndk-aide-release/busybox-arm-$RELEASE
 cp $TOP/ndkinstall.sh $TOP/ndk-aide-release/ndkinstall.sh-$RELEASE
