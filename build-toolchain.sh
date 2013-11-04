50389afb7bfb9eb9fb4be392575d62189c32f967
Hans Kratz <hans@appfour.com>
2013-11-04 13:12:29 +0000

    support for NDK running on Android x86

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -7,6 +7,6 @@ TOP=$(cd $SCRIPTDIR; pwd)
 NDK=$TOP/platform/ndk
 NDK_LOGFILE=/tmp/ndk-build.log
 export NDK NDK_LOGFILE NUM_JOBS
-rm $NDK_LOGFILE
-$NDK/build/tools/rebuild-all-prebuilt.sh --arch=arm --systems=linux-x86 --package-dir=$TOP/ndk-prebuilt $TOP/toolchain-sources/
-$NDK/build/tools/rebuild-all-prebuilt.sh --systems=armstatic --arch=arm --package-dir=$TOP/ndk-prebuilt $TOP/toolchain-sources/
+rm -f $NDK_LOGFILE
+$NDK/build/tools/rebuild-all-prebuilt.sh --arch="arm x86" --systems=androidx86static --package-dir=$TOP/ndk-prebuilt $TOP/toolchain-sources/
+$NDK/build/tools/rebuild-all-prebuilt.sh --systems=androidarmstatic --arch=arm --package-dir=$TOP/ndk-prebuilt $TOP/toolchain-sources/
--- package-ndk-release.sh
+++ package-ndk-release.sh
@@ -6,4 +6,5 @@ NDK=$TOP/platform/ndk
 NDK_LOGFILE=/tmp/ndk-build.log
 export NDK NDK_LOGFILE NUM_JOBS
 rm $NDK_LOGFILE
-$NDK/build/tools/package-release.sh --prebuilt-dir=$TOP/ndk-prebuilt --release=aide --systems=linux-arm --arch=arm --out-dir=$TOP/ndk-release
+$NDK/build/tools/package-release.sh --prebuilt-dir=$TOP/ndk-prebuilt --release=aide --systems=linux-x86 --arch=x86 --toolchains=x86-4.6 --out-dir=$TOP/ndk-release
+$NDK/build/tools/package-release.sh --prebuilt-dir=$TOP/ndk-prebuilt --release=aide --systems=linux-arm --arch=arm --toolchains=arm-linux-androideabi-4.6 --out-dir=$TOP/ndk-release
