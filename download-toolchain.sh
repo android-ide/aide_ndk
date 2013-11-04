5923277091b899a1e82e0306ac79caf8304fba16
Hans Kratz <hans@appfour.com>
2013-11-04 12:33:13 +0000

    Download toolchain sources from a specific date so they match the rest of the SDK soruces.

--- download-toolchain.sh
+++ download-toolchain.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 SCRIPTDIR=$(dirname $0)
 TOP=$(cd $SCRIPTDIR; pwd)
 NDK=$TOP/platform/ndk
 NDK_LOGFILE=/tmp/ndk-build.log
 export NDK NDK_LOGFILE
-mkdir $TOP/toolchain-sources
-$NDK/build/tools/download-toolchain-sources.sh $TOP/toolchain-sources
+mkdir -p $TOP/toolchain-sources
+$NDK/build/tools/download-toolchain-sources.sh --git-date=2013-04-10 $TOP/toolchain-sources
