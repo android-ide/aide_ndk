a2773cd900e1f722af63b69b6a6fa94d8d068120
Hans Kratz <hans@appfour.com>
2017-08-07 16:40:05 +0200

    compile busybox with clang and NDK r15 for Android O

--- build-busybox-arm.sh
+++ build-busybox-arm.sh
@@ -5,5 +5,8 @@ set -e
 SCRIPTDIR=$(dirname $0)
 TOP=$(cd $SCRIPTDIR; pwd)
 cd $TOP/busybox
+$ANDROID_NDK/build/tools/make_standalone_toolchain.py --arch arm --api 14 --install-dir /tmp/my-android-toolchain --force
+export PATH=/tmp/my-android-toolchain/bin:$PATH
+git clean -fdx
 make aide_defconfig
-LDFLAGS="--static" make -j 4 CROSS_COMPILE=arm-linux-gnueabi-
+make EXTRA_CFLAGS="-O0 -D__NDK_MAJOR__=15 -Wno-ignored-optimization-argument -Wno-unused-command-line-argument" CROSS_COMPILE=arm-linux-androideabi- CC=arm-linux-androideabi-clang
--- build-busybox-x86.sh
+++ build-busybox-x86.sh
@@ -5,5 +5,8 @@ set -e
 SCRIPTDIR=$(dirname $0)
 TOP=$(cd $SCRIPTDIR; pwd)
 cd $TOP/busybox
+$ANDROID_NDK/build/tools/make_standalone_toolchain.py --arch x86 --api 14 --install-dir /tmp/my-android-toolchain --force
+export PATH=/tmp/my-android-toolchain/bin:$PATH
+git clean -fdx
 make aide_defconfig
-CFLAGS="-m32" LDFLAGS="-m32 --static" make -j 4
+make EXTRA_CFLAGS="-DFAST_FUNC= -D__NDK_MAJOR__=15 -Wno-ignored-optimization-argument -Wno-unused-command-line-argument" CROSS_COMPILE=i686-linux-android- CC=i686-linux-android-clang
