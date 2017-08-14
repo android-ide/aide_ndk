c20d067a1376627226d08a44b52d0b71587837c9
Hans Kratz <hans@appfour.com>
2017-08-14 22:20:52 +0200

    remove prerequisites for cross compilation

--- install-prerequisites-ubuntu.sh
+++ install-prerequisites-ubuntu.sh
@@ -4,4 +4,4 @@ SCRIPTDIR=$(dirname $0)
 TOP=$(cd $SCRIPTDIR; pwd)
 
 aptitude install git build-essential rsync bison flex curl texinfo libtinfo-dev \
-libncurses5-dev gcc-arm-linux-gnueabi g++-arm-linux-gnueabi xapt
+libncurses5-dev
