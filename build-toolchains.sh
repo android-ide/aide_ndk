c10fc0da1882e3460e4563dd4784ce782800003b
Hans Kratz <hans@appfour.com>
2017-09-04 12:45:13 +0200

    new toolchain build script (WIP)

--- build-toolchains.sh
+++ build-toolchains.sh
@@ -11,6 +11,14 @@ export NDK NDK_LOGFILE ANDROID_BUILD_TOP
 rm -f $NDK_LOGFILE
 rm -f $TOP/ndk-prebuilts/*
 
-$NDK/build/tools/rebuild-all-prebuilt.sh --arch="arm x86" --systems=androidx86static --gcc-version-list=4.9 --llvm-version-list= --package-dir=$TOP/ndk-prebuilt $TOP/toolchain/
+API_LEVEL=14
 
-$NDK/build/tools/rebuild-all-prebuilt.sh --arch="arm x86" --systems=androidarmstatic --gcc-version-list=4.9 --llvm-version-list= --no-gen-platforms --package-dir=$TOP/ndk-prebuilt $TOP/toolchain/
+export HOST_CFLAGS="-D__NDK_MAJOR__=15 -D__ANDROID_API__=$API_LEVEL -march=armv7-a -mthumb -mfloat-abi=softfp -mfpu=vfpv3-d16 -fPIE"
+export HOST_LDFLAGS="-pie -fPIE"
+
+#$ANDROID_NDK/build/tools/make_standalone_toolchain.py --arch arm --api $API_LEVEL --install-dir /tmp/my-android-toolchain --force
+export PATH=/tmp/my-android-toolchain/bin:$PATH
+
+$NDK/build/tools/rebuild-all-prebuilt.sh --arch="arm x86" --systems=android-arm --gcc-version-list=4.9 --llvm-version-list= --no-gen-platforms --package-dir=$TOP/ndk-prebuilt $TOP/toolchain/
+
+##$NDK/build/tools/rebuild-all-prebuilt.sh --arch="arm x86" --systems=android-x86 --gcc-version-list=4.9 --llvm-version-list= --package-dir=$TOP/ndk-prebuilt $TOP/toolchain/
